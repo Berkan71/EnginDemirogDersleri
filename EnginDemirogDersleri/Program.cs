c6013732e127008321e041d3c926ab432cee26ac
berkanil.kizmaz <berkanil.kizmaz@cfn.com.tr>
2021-01-18 20:03:48 +0300

    Add project files.

---
+++ EnginDemirogDersleri/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnginDemirogDersleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product { };
+            product.Adi = "Elma";
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(product);
+        }
+    }
+}
---
+++ EnginDemirogDersleri/SepetManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnginDemirogDersleri
+{
+    class SepetManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Sepete Eklendi.."+ product.Adi);
+        }
+    }
+}
